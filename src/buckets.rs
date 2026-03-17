278218b8f181f5f8f3b8026b06cf8044d8350bcb
Radoslaw Grzanka <radoslawg@gmail.com>
2026-03-17 21:56:23 +0100

    ✨Implement multipart upload Closes #30

--- src/buckets.rs
+++ src/buckets.rs
@@ -1,7 +1,10 @@
 use std::path::Path;
 
 use anyhow::{Context, Result};
-use aws_sdk_s3::types::Bucket;
+use aws_sdk_s3::{
+    primitives::ByteStream,
+    types::{Bucket, CompletedMultipartUpload, CompletedPart},
+};
 use tokio::{fs::File, io::AsyncReadExt};
 
 use crate::config::StorageSettings;
@@ -17,11 +20,108 @@ pub async fn upload_file(filepath: &Path, configuration: &StorageSettings) -> Re
         String::from(filename.to_str().context("Cannot convert to str")?)
     );
     log::debug!("Uploading: {}", destination);
-    upload_to_bucket(filepath, &configuration.bucket, &destination).await;
+    let file_size = tokio::fs::File::open(filename)
+        .await?
+        .metadata()
+        .await?
+        .len();
+
+    if file_size > CHUNK_SIZE as u64 {
+        upload_to_bucket_multipart(filepath, &configuration.bucket, &destination).await?;
+    } else {
+        upload_to_bucket(filepath, &configuration.bucket, &destination).await?;
+    }
     Ok(())
 }
 
-async fn upload_to_bucket(filename: &Path, bucket_name: &String, destination: &String) {
+const CHUNK_SIZE: usize = 10 * 1024 * 1024; // 10 MB
+
+// https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/rustv1/examples/s3/src/bin/s3-multipart-upload.rs#L48
+async fn upload_to_bucket_multipart(
+    filename: &Path,
+    bucket_name: &String,
+    destination: &String,
+) -> Result<()> {
+    log::debug!("Uploading with multipart: {}", destination);
+    let config = aws_config::from_env().load().await;
+    let client = aws_sdk_s3::Client::new(&config);
+
+    let multipart_upload = client
+        .create_multipart_upload()
+        .key(destination)
+        .bucket(bucket_name)
+        .send()
+        .await?;
+
+    let upload_id = multipart_upload.upload_id().context("No upload ID")?;
+
+    let mut file = tokio::fs::File::open(filename).await?;
+    let file_size = file.metadata().await?.len();
+    let mut completed_parts = Vec::new();
+    let mut part_number = 1;
+    let mut uploaded_bytes = 0;
+
+    while uploaded_bytes < file_size {
+        let mut buffer = vec![0u8; CHUNK_SIZE];
+        let bytes_read = file.read(&mut buffer).await?;
+        if bytes_read == 0 {
+            break;
+        }
+
+        if bytes_read < CHUNK_SIZE {
+            buffer.truncate(bytes_read);
+        }
+
+        let stream = ByteStream::from(buffer);
+
+        log::debug!(
+            "Uploading part {} ({} bytes) for {}",
+            part_number,
+            bytes_read,
+            destination
+        );
+
+        let upload_part = client
+            .upload_part()
+            .key(destination)
+            .bucket(bucket_name)
+            .upload_id(upload_id)
+            .body(stream)
+            .part_number(part_number)
+            .send()
+            .await?;
+
+        completed_parts.push(
+            CompletedPart::builder()
+                .e_tag(upload_part.e_tag().unwrap_or_default())
+                .part_number(part_number)
+                .build(),
+        );
+
+        uploaded_bytes += bytes_read as u64;
+        part_number += 1;
+    }
+    let completed_multipart_upload: CompletedMultipartUpload = CompletedMultipartUpload::builder()
+        .set_parts(Some(completed_parts))
+        .build();
+
+    client
+        .complete_multipart_upload()
+        .key(destination)
+        .bucket(bucket_name)
+        .multipart_upload(completed_multipart_upload)
+        .upload_id(upload_id)
+        .send()
+        .await?;
+
+    Ok(())
+}
+
+async fn upload_to_bucket(
+    filename: &Path,
+    bucket_name: &String,
+    destination: &String,
+) -> Result<()> {
     // Load the AWS configuration.
     // `aws_config::from_env()` builds a configuration based on environment variables
     // (e.g., AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION).
@@ -34,17 +134,14 @@ async fn upload_to_bucket(filename: &Path, bucket_name: &String, destination: &S
         file.read_to_end(&mut buffer).await.expect("Read file ok");
     }
 
-    match client
+    client
         .put_object()
         .bucket(bucket_name)
         .key(destination)
         .body(buffer.into())
         .send()
-        .await
-    {
-        Ok(_) => log::info!("Upload successfull to bucket: {:?}", bucket_name),
-        Err(err) => log::error!("Error uploading object: {:?}", err),
-    };
+        .await?;
+    Ok(())
 }
 
 async fn get_bucket(
